caad1af6f392b96d47e497613a1cce40d23325c0
aa18514 <aa18514@github.com>
2018-08-07 19:52:32 +0100

    cleanup

--- electricity_demand_generation/electricity_demand.py
+++ electricity_demand_generation/electricity_demand.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 from typing import List
+from collections import OrderedDict
+
 Vector_int = List[int]
 
 
@@ -10,22 +12,43 @@ DATE_STR = ['Jan', 'Feb', 'March', 'April',
             'May', 'Jun', 'Jul', 'Aug',
             'Sept', 'Oct', 'Nov', 'Dec']
 
+ELECTRICITY_YEARS = {
+        2017: {},
+        2016: {},
+        2015: {},
+        2014: {},
+        2013: {},
+        2012: {},
+        2011: {}
+    }
 
-def return_file_attributes(path: str, key: str) -> str:
+
+def load_file_attributes(path: str, nd_key: str, settlement_key: str) -> str:
     """return electricity consumption in MW,
     and the settlement dates"""
     reader = csv.DictReader(open(path, 'r'))
-    dict_list = []
-    dates = []
+    months = []
     for line in reader:
-        val = datetime.strptime(line['SETTLEMENT_DATE'], '%d/%m/%Y')
-        if val.year == 2017:
-            dict_list.append((line[key]))
-            dates.append(val.month)
+        val = datetime.strptime(line[settlement_key], '%d/%m/%Y')
+        months.append(val.month)
+        if val not in ELECTRICITY_YEARS[val.year]:
+                ELECTRICITY_YEARS[val.year][val] = [int(line[nd_key])]
+        else:
+                ELECTRICITY_YEARS[val.year][val].append(int(line[nd_key]))
+    print(months)
+    return np.array(months)
+
+
+def query_data(year: int)->int:
+    ordered_dict = OrderedDict(sorted(ELECTRICITY_YEARS[year].items(),
+                                      key=lambda t: t[0]))
+    dict_list = []
+    data = list(ordered_dict.values())
+    for sublist in data:
+        for val in sublist:
+            dict_list.append(val)
     dict_list = np.array(dict_list, dtype=int)
-    dict_list = dict_list[np.argsort(dates)]
-    dates = np.array(dates)
-    dates = np.sort(dates)
+    dates = np.array(list(ordered_dict.keys()))
     return dict_list, dates
 
 
@@ -60,14 +83,17 @@ def print_data_summary(data: Vector_int, dt_index: Vector_int, granularity_const
     plt.plot(x, mean)
 
 
-def decode_electricity_data(path: str, net_demand: str)->str:
-    data, dt_sorted = return_file_attributes(path, net_demand)
-    dt_index = np.where(dt_sorted[:-1] != dt_sorted[1:])[0] + 1
+def decode_electricity_data(path: str, net_demand: str, settlement_date: str)->str:
+    dt_index = load_file_attributes(path, net_demand, settlement_date)
+    dt_index = np.sort(dt_index)
+    dt_index = np.where(dt_index[:-1] != dt_index[1:])[0]
+    data, dt_sorted = query_data(2017)
     dt_index = np.append(0, dt_index)
     dt_index = np.append(dt_index, len(data) - 1)
-    print_data_summary(data, dt_index, 1)
+    visualize_data(data)
+    partition_data(dt_index)
     plt.show()
 
 
 if __name__ == "__main__":
-    decode_electricity_data("DemandData_2017.csv", 'ND')
+    decode_electricity_data("DemandData_2017.csv", 'ND', 'SETTLEMENT_DATE')
