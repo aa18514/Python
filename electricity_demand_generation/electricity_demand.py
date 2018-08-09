634c08c0179ebfe29fdf98bbc0e3cc06a9a3c876
aa18514 <aa18514@github.com>
2018-08-09 13:17:37 +0100

    add dickey-fuller test

--- electricity_demand_generation/electricity_demand.py
+++ electricity_demand_generation/electricity_demand.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from typing import List
 from collections import OrderedDict
 from numpy import log
+from statsmodels.tsa.stattools import adfuller
 
 Vector_int = List[int]
 
@@ -71,17 +72,20 @@ def query_data(year: int)->int:
 
 def visualize_data(e_consumption: Vector_int,
                    title: str, x_label: str,
-                   y_label: str)->(Vector_int, str):
+                   y_label: str,
+                   label: str)->(Vector_int, str):
     """
     @data: electricity consumption in MW
     """
-    plt.figure()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     x = np.arange(0, len(e_consumption))
     plt.xticks(np.arange(min(x), max(x) + 1, 1500), DATE_STR)
-    plt.plot(x, e_consumption)
+    plt.plot(x, e_consumption, label=label)
+    ax.legend(loc='lower left')
 
 
 def partition_data(dt_index: Vector_int)->Vector_int:
@@ -109,26 +113,39 @@ def pre_process(data: Vector_int, interval: int)->(Vector_int, int):
     return data
 
 
+def dickey_fuller_test(series: Vector_int)->Vector_int:
+    """
+    :param series: time series data
+    :return: p value, a p-value less than 0.05 indicates that the hypothesis H0,
+    i.e the series is stationary should be accepted
+    """
+    s_test = adfuller(series, autolag='AIC')
+    return s_test[1]
+
+
 def decode_electricity_data(path: str, net_demand: str, settlement_date: str)->str:
     dt_index = load_file_attributes(path, net_demand, settlement_date)
     dt_index = np.sort(dt_index)
     dt_index = np.where(dt_index[:-1] != dt_index[1:])[0]
     data, dt_sorted = query_data(2017)
     visualize_data(data,
-                   'time series - original',
+                   'NATIONAL GRID time series - original',
                    'time in 2017',
+                   'electricity consumption in MW',
                    'original features')
     dt_index = np.append(0, dt_index)
     dt_index = np.append(dt_index, len(data) - 1)
     partition_data(dt_index)
-    data = pre_process(data, 2)
+    data = pre_process(data, 1)
     visualize_data(data,
-                   'de-trended and de-seasonalized time series',
+                   'NATIONAL GRID de-trended and de-seasonalized time series',
                    'time in 2017',
-                   'modified features')
+                   'modified features',
+                   'sixth-order log differencing')
     partition_data(dt_index)
     plt.show()
-
+    p_val = dickey_fuller_test(data)
+    print(p_val)
 
 if __name__ == "__main__":
     decode_electricity_data("DemandData_2017.csv", 'ND', 'SETTLEMENT_DATE')
