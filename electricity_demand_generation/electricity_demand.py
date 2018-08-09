bb4a2b3eceeec0f88308798a5e4e67dc55419cda
aa18514 <aa18514@github.com>
2018-08-09 01:09:56 +0100

    remove trend and seasonility from data, prepare data for ARIMA modelling

--- electricity_demand_generation/electricity_demand.py
+++ electricity_demand_generation/electricity_demand.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime
 from typing import List
 from collections import OrderedDict
-import pickle
+from numpy import log
 
 Vector_int = List[int]
 
@@ -23,7 +23,22 @@ ELECTRICITY_YEARS = {
         2011: {}
     }
 
-ORDERED_DICT = None
+
+def remove_seasonality(data: Vector_int)->Vector_int:
+    return log(data)
+
+
+def remove_trend(x_data: Vector_int)->Vector_int:
+    """
+    :param x_data: time series data
+     first-order differencing to remove trend
+    :return: de-trended series
+    """
+    diff = list()
+    for i in range(1, len(x_data)):
+        value = x_data[i] - x_data[i - 1]
+        diff.append(value)
+    return diff
 
 
 def load_file_attributes(path: str, nd_key: str, settlement_key: str) -> str:
@@ -38,14 +53,13 @@ def load_file_attributes(path: str, nd_key: str, settlement_key: str) -> str:
                 ELECTRICITY_YEARS[val.year][val] = [int(line[nd_key])]
         else:
                 ELECTRICITY_YEARS[val.year][val].append(int(line[nd_key]))
-    print(months)
     return np.array(months)
 
 
 def query_data(year: int)->int:
-    dict_list = []
     ordered_dict = OrderedDict(sorted(ELECTRICITY_YEARS[year].items(),
                                       key=lambda t: t[0]))
+    dict_list = []
     data = list(ordered_dict.values())
     for sublist in data:
         for val in sublist:
@@ -55,14 +69,17 @@ def query_data(year: int)->int:
     return dict_list, dates
 
 
-def visualize_data(e_consumption: Vector_int)->Vector_int:
+def visualize_data(e_consumption: Vector_int,
+                   title: str, x_label: str,
+                   y_label: str)->(Vector_int, str):
     """
     @data: electricity consumption in MW
     """
-    plt.xlabel('time in 2017')
-    plt.ylabel('electricity consumption/MW')
+    plt.figure()
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     x = np.arange(0, len(e_consumption))
-    plt.figure(1)
     plt.xticks(np.arange(min(x), max(x) + 1, 1500), DATE_STR)
     plt.plot(x, e_consumption)
 
@@ -81,16 +98,15 @@ def print_data_summary(data: Vector_int, dt_index: Vector_int, granularity_const
     for i in range(0, len(dt_index) - 1):
         mean.append(np.mean(data[dt_index[i]:dt_index[i+1]]))
     x = np.arange(0, len(mean))
-    plt.figure(2)
     plt.xticks(np.arange(min(x), max(x) + 1, granularity_constant), DATE_STR)
     plt.plot(x, mean)
 
 
-def write_to_pkl(year: int, file_path: str)->(int, str):
-    ordered_dict = OrderedDict(sorted(ELECTRICITY_YEARS[year].items(),
-                                      key=lambda t: t[0]))
-    with open(file_path, "wb") as f:
-        pickle.dump(ordered_dict, f)
+def pre_process(data: Vector_int, interval: int)->(Vector_int, int):
+    data = remove_seasonality(data)
+    for i in range(interval):
+        data = remove_trend(data)
+    return data
 
 
 def decode_electricity_data(path: str, net_demand: str, settlement_date: str)->str:
@@ -98,15 +114,20 @@ def decode_electricity_data(path: str, net_demand: str, settlement_date: str)->s
     dt_index = np.sort(dt_index)
     dt_index = np.where(dt_index[:-1] != dt_index[1:])[0]
     data, dt_sorted = query_data(2017)
+    visualize_data(data,
+                   'time series - original',
+                   'time in 2017',
+                   'original features')
     dt_index = np.append(0, dt_index)
     dt_index = np.append(dt_index, len(data) - 1)
-    visualize_data(data)
+    partition_data(dt_index)
+    data = pre_process(data, 2)
+    visualize_data(data,
+                   'de-trended and de-seasonalized time series',
+                   'time in 2017',
+                   'modified features')
     partition_data(dt_index)
     plt.show()
-    years = [2011, 2012, 2013, 2014, 2015, 2016, 2017]
-    for year in years:
-        if len(ELECTRICITY_YEARS[year].keys()) > 0:
-            write_to_pkl(year, "data_" + str(year) + ".pkl")
 
 
 if __name__ == "__main__":
