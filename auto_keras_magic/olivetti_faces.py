502fbe33c387b1deab65b11e8b7a8b87289cd499
aa18514 <aa18514@github.com>
2018-08-11 19:01:39 +0100

    add classification report

--- auto_keras_magic/olivetti_faces.py
+++ auto_keras_magic/olivetti_faces.py
@@ -1,15 +1,16 @@
-from sklearn.datasets import fetch_mldata
-from sklearn.datasets import fetch_olivetti_faces
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 import autokeras as ak
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.datasets import fetch_mldata
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.metrics import classification_report
 
 if __name__ == "__main__":
     olivetti_faces = fetch_olivetti_faces()
     x, y = olivetti_faces['data'], olivetti_faces['target']
     x = x.reshape(len(y), 64, 64)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25)
     X_train = X_train.reshape(X_train.shape + (1,))
     X_test = X_test.reshape(X_test.shape + (1,))
     clf = ak.ImageClassifier(verbose=True)
@@ -18,3 +19,4 @@ if __name__ == "__main__":
     test_accuracy = np.sum(y_pred == y_test)
     test_accuracy = 100 * test_accuracy/len(y_pred)
     print("test accuracy %f: " % test_accuracy)
+    print(classification_report(y_test, y_pred))
